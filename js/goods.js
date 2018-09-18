ca19eae06872c7ae580271d3c9b10feef117b0be
Anna-Teshkina <anna_teshkina@mail.ru>
2018-09-18 18:40:09 +0300

    Bags were fixed. Function getShuffleArray was created.

--- js/goods.js
+++ js/goods.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var NUMBER_CARDS = 10;
-
 var CARD_NAMES = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
 var PIC_NAMES = ['gum-cedar.jpg', 'gum-chile.jpg', 'gum-eggplant.jpg', 'gum-mustard.jpg', 'gum-portwine.jpg', 'gum-wasabi.jpg', 'ice-cucumber.jpg', 'ice-eggplant.jpg', 'ice-garlic.jpg', 'ice-italian.jpg', 'ice-mushroom.jpg', 'ice-pig.jpg', 'marmalade-beer.jpg', 'marmalade-caviar.jpg', 'marmalade-corn.jpg', 'marmalade-new-year.jpg', 'marmalade-sour.jpg', 'marshmallow-bacon.jpg', 'marshmallow-beer.jpg', 'marshmallow-shrimp.jpg', 'marshmallow-spicy.jpg', 'marshmallow-wine.jpg', 'soda-bacon.jpg', 'soda-celery.jpg', 'soda-cob.jpg', 'soda-garlic.jpg', 'soda-peanut-grapes.jpg', 'soda-russian.jpg'];
 var CONTENT_NAMES = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор бекона', 'ароматизатор свинца', 'ароматизатор дуба, идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор', 'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
-
-var cards = [];
+var ORDER_COUNT = 3; // количество товаров, добавленных в корзину
 
 var getRandomElement = function (array) {
   var randomElement = array[Math.floor(Math.random() * array.length)];
@@ -18,7 +16,8 @@ var getRandomFromInterval = function (min, max) {
   return randomFromInterval;
 };
 
-var getCardsArray = function (n, array) {
+var getCardsArray = function (n) {
+  var array = [];
   for (var i = 0; i < n; i++) {
     var cardItem = getCard();
     array.push(cardItem);
@@ -26,40 +25,36 @@ var getCardsArray = function (n, array) {
   return array;
 };
 
+var getShuffleArray = function (array) {
+  array = [];
+  var j;
+  var temp;
+  for (var i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+  }
+  return array;
+};
+
 var getContents = function () {
   var componentsArray = [];
-  var components = '';
   var componentsNumber = getRandomFromInterval(1, CONTENT_NAMES.length);
 
+  getShuffleArray(CONTENT_NAMES);
+
   for (var i = 0; i < componentsNumber; i++) {
-    var componentItem = getRandomElement(CONTENT_NAMES);
-
-    if (componentsArray.length > 0) {
-      while (componentsArray.includes(componentItem)) {
-        componentItem = getRandomElement(CONTENT_NAMES);
-      }
-    }
-    componentsArray.push(componentItem);
+    componentsArray[i] = CONTENT_NAMES[i];
   }
-
-  for (i = 0; i < componentsArray.length; i++) {
-    if (i !== componentsArray.length - 1) {
-      components += componentsArray[i] + ', ';
-    } else {
-      components += componentsArray[i];
-    }
-  }
+  var components = componentsArray.join(', ');
   return components;
 };
 
 var getCard = function () {
-  var cardName = getRandomElement(CARD_NAMES);
-  var cardUrl = 'img/cards/' + getRandomElement(PIC_NAMES);
-  var booleanRandom = Math.round(Math.random());
-
   var card = {
-    name: cardName,
-    picture: cardUrl,
+    name: getRandomElement(CARD_NAMES),
+    picture: 'img/cards/' + getRandomElement(PIC_NAMES),
     amount: getRandomFromInterval(0, 20),
     price: getRandomFromInterval(100, 1500),
     weight: getRandomFromInterval(30, 300),
@@ -68,7 +63,7 @@ var getCard = function () {
       number: getRandomFromInterval(10, 900),
     },
     nutritionFacts: {
-      sugar: booleanRandom,
+      sugar: Math.round(Math.random()),
       energy: getRandomFromInterval(70, 500),
       contents: getContents()
     }
@@ -77,7 +72,7 @@ var getCard = function () {
   return card;
 };
 
-getCardsArray(NUMBER_CARDS, cards);
+var cards = getCardsArray(NUMBER_CARDS);
 
 document.querySelector('.catalog__cards').classList.remove('catalog__cards--load');
 document.querySelector('.catalog__load').classList.add('visually-hidden');
@@ -133,11 +128,9 @@ var renderCard = function (card) {
 
   cardElement.querySelector('.star__count').textContent = '(' + card.rating.number + ')';
 
-  var sugarContains = '';
+  var sugarContains = 'Не содержит сахар. ';
   if (card.nutritionFacts.sugar) {
     sugarContains = 'Содержит сахар. ';
-  } else {
-    sugarContains = 'Не содержит сахар. ';
   }
 
   cardElement.querySelector('.card__characteristic').textContent = sugarContains + card.nutritionFacts.energy + ' ккал';
@@ -146,7 +139,8 @@ var renderCard = function (card) {
   return cardElement;
 };
 
-var renderFragment = function (n, method, array) {
+var renderFragment = function (n, method, arrayName) {
+  var array = arrayName;
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < n; i++) {
     fragment.appendChild(method(array[i]));
@@ -157,11 +151,7 @@ var renderFragment = function (n, method, array) {
 cardList.appendChild(renderFragment(cards.length, renderCard, cards));
 
 // Шаблон для товара, добавленного в корзину
-var ORDER_COUNT = 3; // количество товаров, добавленных в корзину
-
-var orders = [];
-
-getCardsArray(ORDER_COUNT, orders);
+var orders = getCardsArray(ORDER_COUNT);
 
 var orderList = document.querySelector('.goods__cards');
 var orderElementTemplate = document.querySelector('#card-order').content.querySelector('.goods_card');
