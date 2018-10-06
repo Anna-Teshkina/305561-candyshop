b9e71f017448484244b02b5b9b2385b55f5aa252
Anna-Teshkina <anna_teshkina@mail.ru>
2018-10-06 08:22:11 +0300

    Form validation

--- js/goods.js
+++ js/goods.js
@@ -488,7 +488,7 @@ var setGoodsInCart = function (e, type) {
 
     if (type === 'add') {
       setQuantityClass(e, cards[btnId].amount); // меняем класс текущей карточки каталога
-      addInCart(orders, currentOrder, flag, q);
+      addInCart(orders, currentOrder, flag, q); // добавляем товар в корзину
     }
     if (type === 'increase') {
       increaseInCart(orders, e);
@@ -520,6 +520,7 @@ var deleteOrder = function (cardsArray, ordersArray, id, ev, orderL) {
     cardsArray[id].amount += v;
     orderL.removeChild(ev);
     showEmptyResults();
+    setDisabledForm(formInputsList, formBtnsList); // блокируем форму заказа
   } else {
     ordersArray.splice(index, 1); // удаляем из заказов текущий объект
     cardsArray[id].amount += parseInt(ev.querySelector('.card-order__count').value, 10);
@@ -545,6 +546,7 @@ var decreaseInCart = function (ordersArray, ev, orderL) {
       orderL.removeChild(ev);
       showEmptyResults();
       // console.log(ordersArray);
+      setDisabledForm(formInputsList, formBtnsList); // блокируем форму заказа
     } else {
       ordersArray.splice(index, 1);
       orderL.removeChild(ev);
@@ -567,6 +569,7 @@ var addInCart = function (ordersArray, curOrder, flag, x) {
     curOrder.amount += 1;
     ordersArray.push(curOrder);
     orderList.appendChild(renderOrder(curOrder));
+    setActiveForm(formInputsList, formBtnsList); // активируем форму для отправки
   } else {
     var currentResult = findOrderInCart(curOrder, ordersArray);
     flag = currentResult.flag;
@@ -614,48 +617,331 @@ var changeSlideValue = function (evt, btnClass) {
   return percent;
 };
 
-// Переключение вкладок в форме оформления заказа
-document.querySelector('#deliver__store').addEventListener('click', function () {
-  document.querySelector('.deliver__store').classList.remove('visually-hidden');
-  document.querySelector('.deliver__courier').classList.add('visually-hidden');
-});
+// алгоритм Луна для проверки корректности воода банковской карты
+var checkCardNumber = function (code) {
+  // var code = '1834567890123456';
+  // console.log('code = ' + code);
+  var codeArray = code.split('');
+  var summElements = 0;
 
-document.querySelector('#deliver__courier').addEventListener('click', function () {
-  document.querySelector('.deliver__courier').classList.remove('visually-hidden');
-  document.querySelector('.deliver__store').classList.add('visually-hidden');
-});
-
-document.querySelector('#payment__card').addEventListener('click', function () {
-  document.querySelector('.payment__card-wrap').classList.remove('visually-hidden');
-  document.querySelector('.payment__cash-wrap').classList.add('visually-hidden');
-});
-
-document.querySelector('#payment__cash').addEventListener('click', function () {
-  document.querySelector('.payment__cash-wrap').classList.remove('visually-hidden');
-  document.querySelector('.payment__card-wrap').classList.add('visually-hidden');
-});
-
-/* алгоритм Луна
-var code = '1834567890123456';
-var codeArray = code.split('');
-var summElements = 0;
-
-for (var i = 0; i < codeArray.length; i++) {
-  if (!(i % 2)) {
-    codeArray[i] *= 2;
-    if (codeArray[i] >= 10) {
-      codeArray[i] -= 9;
+  for (var i = 0; i < codeArray.length; i++) {
+    if (!(i % 2)) {
+      codeArray[i] *= 2;
+      if (codeArray[i] >= 10) {
+        codeArray[i] -= 9;
+      }
     }
+    summElements += parseInt(codeArray[i], 10);
   }
 
-  summElements += parseInt(codeArray[i]);
+  // console.log('codeArray = ' + codeArray);
+  // console.log('summElements = ' + summElements);
+  var result = false; // изначально предполагаем, что сумма не кратна 10
+  if (summElements % 10 === 0) {
+    // console.log('Кратно 10');
+    result = true;
+  }
+  return result;
+};
+
+// 7.3 Если в корзине нет ни одного товара, форма оформления заказа блокируется
+// с помощью добавления атрибута disabled для всех полей или для блоков fieldset,
+// которые их содержат.
+
+var paymentBlock = document.querySelector('.payment');
+var deliverBlock = document.querySelector('.deliver');
+
+var paymentsInputs = paymentBlock.querySelectorAll('.text-input__input');
+var deliveryRadioBtns = deliverBlock.querySelectorAll('.input-btn__input');
+var deliveryInputs = deliverBlock.querySelectorAll('.text-input__input');
+
+var formInputsList = document.querySelectorAll('.text-input__input');
+var formBtnsList = document.querySelectorAll('.input-btn__input');
+
+var setDisabledForm = function (inputsList, btnsList) {
+  for (var i = 0; i < inputsList.length; i++) {
+    inputsList[i].setAttribute('disabled', 'disabled');
+  }
+
+  for (i = 0; i < btnsList.length; i++) {
+    btnsList[i].setAttribute('disabled', 'disabled');
+  }
+
+  document.querySelector('.deliver__textarea').setAttribute('disabled', 'disabled');
+
+  document.querySelector('.buy__submit-btn').setAttribute('disabled', 'disabled');
+
+  // console.log('Поля формы заблокированы');
+  toggleInfoTabActive();
+};
+
+var setActiveForm = function (inputsList, btnsList) {
+  for (var i = 0; i < inputsList.length; i++) {
+    inputsList[i].removeAttribute('disabled', 'disabled');
+  }
+
+  for (i = 0; i < btnsList.length; i++) {
+    btnsList[i].removeAttribute('disabled', 'disabled');
+  }
+
+  document.querySelector('.buy__submit-btn').removeAttribute('disabled', 'disabled');
+
+  // если пользователь еще не добавил никакой товар в корзину, но уже
+  // поинтересовался какие данные ему нужно будет предоставить - нажимал
+  // на клавиши табов, в момент активации формы (корзина не пуста), надо
+  // сделать так, чтобы при активации формы активировались поля с активными табами,
+  // а с неактивными - деактивировались.
+  if (document.querySelector('#payment__card').hasAttribute('checked')) {
+    setBankingPayment();
+  }
+  if (document.querySelector('#payment__cash').hasAttribute('checked')) {
+    disabledBankingPayment();
+  }
+  if (document.querySelector('#deliver__store').hasAttribute('checked')) {
+    setStoreDelivery();
+  }
+  if (document.querySelector('#deliver__courier').hasAttribute('checked')) {
+    setCourierDelivery();
+  }
+
+  toggleInfoTabActive();
+};
+
+var toggleChecked = function (block) {
+  var variantsAll = block.querySelectorAll('.toggle-btn__input');
+
+  for (var i = 0; i < variantsAll.length; i++) {
+    variantsAll[i].addEventListener('click', function (evt) {
+      for (var j = 0; j < variantsAll.length; j++) {
+        if (variantsAll[j].hasAttribute('checked')) {
+          variantsAll[j].removeAttribute('checked');
+        }
+      }
+      evt.currentTarget.setAttribute('checked', 'checked');
+
+      // я подумала, что нужно оставить пользователю возможность
+      // перемещаться по вкладкам, но при пустой корзине поля останутся заблокированными
+      switch (evt.currentTarget.getAttribute('value')) {
+        case 'card':
+          document.querySelector('.payment__card-wrap').classList.remove('visually-hidden');
+          document.querySelector('.payment__cash-wrap').classList.add('visually-hidden');
+          break;
+        case 'cash':
+          document.querySelector('.payment__cash-wrap').classList.remove('visually-hidden');
+          document.querySelector('.payment__card-wrap').classList.add('visually-hidden');
+          break;
+        case 'store':
+          document.querySelector('.deliver__store').classList.remove('visually-hidden');
+          document.querySelector('.deliver__courier').classList.add('visually-hidden');
+          break;
+        case 'courier':
+          document.querySelector('.deliver__courier').classList.remove('visually-hidden');
+          document.querySelector('.deliver__store').classList.add('visually-hidden');
+          break;
+      }
+    });
+  }
+};
+
+var toggleInfoTabActive = function () {
+  var variantsAll = document.querySelectorAll('.toggle-btn__input');
+
+  for (var i = 0; i < variantsAll.length; i++) {
+    variantsAll[i].addEventListener('click', function (evt) {
+      if (orders.length !== 0) {
+        switch (evt.currentTarget.getAttribute('value')) {
+          case 'card':
+            setBankingPayment();
+            break;
+          case 'cash':
+            disabledBankingPayment();
+            break;
+          case 'store':
+            setStoreDelivery();
+            break;
+          case 'courier':
+            setCourierDelivery();
+            break;
+        }
+        return false;
+      } else {
+        return false;
+      }
+    });
+  }
+};
+
+setDisabledForm(formInputsList, formBtnsList); // изначально корзина пуста - блокируем все поля ввода
+toggleChecked(paymentBlock);
+toggleChecked(deliverBlock);
+
+var disabledBankingPayment = function () {
+  for (var i = 0; i < paymentsInputs.length; i++) {
+    paymentsInputs[i].setAttribute('disabled', 'disabled');
+  }
+};
+
+var setBankingPayment = function () {
+  for (var i = 0; i < paymentsInputs.length; i++) {
+    paymentsInputs[i].removeAttribute('disabled', 'disabled');
+  }
+};
+
+var setStoreDelivery = function () {
+  for (var i = 0; i < deliveryRadioBtns.length; i++) {
+    deliveryRadioBtns[i].removeAttribute('disabled', 'disabled');
+  }
+  for (i = 0; i < deliveryInputs.length; i++) {
+    deliveryInputs[i].setAttribute('disabled', 'disabled');
+  }
+  document.querySelector('.deliver__textarea').setAttribute('disabled', 'disabled');
+  chooseMetro();
+};
+
+var setCourierDelivery = function () {
+  for (var i = 0; i < deliveryRadioBtns.length; i++) {
+    deliveryRadioBtns[i].setAttribute('disabled', 'disabled');
+  }
+  for (i = 0; i < deliveryInputs.length; i++) {
+    deliveryInputs[i].removeAttribute('disabled', 'disabled');
+  }
+  document.querySelector('.deliver__textarea').removeAttribute('disabled', 'disabled');
+};
+
+// 9.4 При переключении станции метро, изображение карты меняется на то, которое соответствует выбранной станции.
+var chooseMetro = function () {
+  var metroVariants = document.querySelectorAll('.input-btn__input');
+  for (var i = 0; i < metroVariants.length; i++) {
+    metroVariants[i].addEventListener('click', function (evt) {
+      for (var j = 0; j < metroVariants.length; j++) {
+        if (metroVariants[j].hasAttribute('checked')) {
+          metroVariants[j].removeAttribute('checked');
+        }
+      }
+      evt.currentTarget.setAttribute('checked', 'checked');
+      var picName = evt.currentTarget.getAttribute('value');
+      document.querySelector('.deliver__store-map-img').src = 'img/map/' + picName + '.jpg';
+    });
+  }
+};
+
+// 8.7 Рядом с номером карты отображается статус карты. Пока данные не введены
+// или не все данные корректны, отображается статус «Не определён».
+// Когда все введённые данные корректны, статус карты меняется на «Одобрен».
+
+for (var i = 0; i < paymentsInputs.length; i++) {
+  paymentsInputs[i].addEventListener('blur', function () {
+    // console.log('Фокус снят');
+    checkCardDetails();
+  });
 }
 
-console.log('codeArray2 = ' + codeArray);
-console.log('summElements = ' + summElements);
+var checkCardDetails = function () {
+  var cardInputCheck = [];
+  for (var k = 0; k < paymentsInputs.length; k++) {
+    // console.log('paymentsInputs.length = ' + paymentsInputs.length);
+    var t = paymentsInputs[k].name;
+    // var cardInputCheck = [];
+    if (t === 'card-number') {
+      var cardNumber = paymentsInputs[k].value;
+      // console.log('cardNumber = ' + cardNumber);
+      if ((!checkCardNumber(cardNumber)) || (cardNumber === '')) {
+        cardInputCheck[0] = 0;
+      } else {
+        cardInputCheck[0] = 1;
+      }
+      // console.log('luna = ' + checkCardNumber(cardNumber));
+      // console.log('cardInputCheck[0] = ' + cardInputCheck[0]);
+    }
+    if (t === 'card-date') {
+      var cardDate = paymentsInputs[k].value;
+      // console.log('cardDate = ' + cardDate);
 
-if (summElements % 10 === 0) {
-  console.log('Кратно 10');
-} else {
-  console.log('Не кратно 10');
-} */
+      // if ((/[0-1]{1}[0-9]{1}\/[0-9]{2}/.test(cardDate)) && (cardDate !== '')) {
+      if (!checkCardDate(cardDate)) {
+        cardInputCheck[1] = 0;
+      } else {
+        cardInputCheck[1] = 1;
+      }
+      // console.log('cardDatecheck = ' + /[0-9]{2}/.test(cardDate));
+      // console.log('cardInputCheck[1] = ' + cardInputCheck[1]);
+    }
+    if (t === 'card-cvc') {
+      var cardCvc = paymentsInputs[k].value;
+      // console.log('cardCvc = ' + cardCvc);
+      if ((/[1-9]{1}[0-9]{2}/.test(cardCvc)) && (cardCvc !== '')) {
+        cardInputCheck[2] = 1;
+      } else {
+        cardInputCheck[2] = 0;
+      }
+      // console.log('cardInputCheck[2] = ' + cardInputCheck[2]);
+    }
+    if (t === 'cardholder') {
+      var cardHolder = paymentsInputs[k].value;
+      /* console.log('cardHolder = ' + cardHolder);*/
+      if (checkHolderName(cardHolder)) {
+        cardInputCheck[3] = 1;
+      } else {
+        cardInputCheck[3] = 0;
+      }
+      // console.log('cardInputCheck[3] = ' + cardInputCheck[3]);
+    }
+  }
+  // console.log('cardInputCheck = ' + cardInputCheck);
+  var status = 1;
+  for (var j = 0; j < cardInputCheck.length; j++) {
+    status *= cardInputCheck[j];
+  }
+
+  var cardStatusNode = document.querySelector('.payment__card-status');
+
+  if (status === 1) {
+    cardStatusNode.textContent = 'Определен';
+  } else {
+    cardStatusNode.textContent = 'Не определен';
+  }
+};
+
+var checkHolderName = function (name) {
+  if (name === '') {
+    return false;
+  }
+  var nameA = name.split(' ');
+  // console.log('nameA = ' + nameA);
+  if (nameA.length !== 2) {
+    return false;
+  }
+  for (var j = 0; j < 2; j++) {
+    if (!/[A-Z]{1,}/.test(nameA[j])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+var checkCardDate = function (date) {
+  if (date === '') {
+    return false;
+  }
+  var dateA = date.split('/');
+  // console.log('dateA = ' + dateA);
+  if (dateA.length !== 2) {
+    return false;
+  }
+
+  var month = dateA[0];
+  var year = dateA[1];
+
+  // console.log('month = ' + month);
+  // console.log('year = ' + year);
+
+  if (month > 12) {
+    return false;
+  }
+
+  if (!/[0-9]{2}/.test(year)) {
+    return false;
+  }
+  return true;
+};
+
